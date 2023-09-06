c5f7e4c1784f42d1c11e0b092d428c3fa8ff3894
Ibrahim Ansari <iamibrahimansari@gmail.com>
2023-09-06 21:19:30 +0530

    remove copyright text from footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () =>{
     return <footer className="footer">
-        <span>Copyright &copy; {new Date().getFullYear()}</span>
+        {/* <span>Copyright &copy; {new Date().getFullYear()}</span> */}
         <span>All rights reserved</span>
     </footer>
 }
